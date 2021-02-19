1c62fa3442650b1c2eb1e4a0ef5a5f2875f9187f
Celine_lhp5 <celine.louvel.76@outlook.fr>
2021-02-19 22:19:37 +0100

    ajout fichier

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="img/svg" href="assets/img/geek.svg">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <!-- bootstrap v5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.2.0/mdb.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Céline Louvel - Portfolio</title>
+</head>
+
+<body>
+
+    <div class="header">
+
+    </div>
+
+    <div class="backIntro">
+
+        <section class="flexbox">
+            <div class="flexchild mx-auto mt-5 row">
+                <figure>
+                    <div class="row">
+                        <div class="col-8">
+                            <p><img src="assets/img/Photo_Celine_Louvel.jpg" class="blob-one--animated" alt="photo Céline Louvel"></p>
+                        </div>
+                        <div class="col-4 mt-5">
+                            <p><i class="fas fa-code"></i> Développeuse web et web mobile en formation. <i class="fas fa-code"></i></p>
+                            <p><i class="fas fa-exclamation-circle"></i> En recherche d'alternance Conception et développement d'applications.</p>
+                        </div>
+                    </div>
+                </figure>
+            </div>
+        </section>
+
+        <p class="justify-content-center container textIntro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum</p>
+
+    </div>
+
+
+
+    <!-- bootstrap v5 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.2.0/mdb.min.js"></script>
+</body>
+
+</html>
