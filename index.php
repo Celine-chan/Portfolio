d10281c5e893a04a034577d83c05d7576122e856
Celine_lhp5 <celine.louvel.76@outlook.fr>
2021-03-02 14:36:33 +0100

    modif portfolio

--- index.php
+++ index.php
@@ -15,9 +15,17 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Shippori+Mincho&display=swap" rel="stylesheet">
     <!-- MDB -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.2.0/mdb.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/style.css">
+    <link href="lightbox2-2.11.3/dist/css/lightbox.min.css" rel="stylesheet" />
+    <script src="lightbox2-2.11.3/dist/js/lightbox-plus-jquery.min.js"></script>
     <title>Céline Louvel - Portfolio</title>
 </head>
 
@@ -25,7 +33,7 @@
 
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Louvel Céline</a>
+            <a class="navbar-brand nameTitle" href="#">Louvel Céline</a>
             <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarRightAlignExample" aria-controls="navbarRightAlignExample" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
@@ -34,17 +42,20 @@
 
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                        <a class="nav-link active fontNav" aria-current="page" href="#">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Mes Réalisations</a>
+                        <a class="nav-link fontNav" href="#presAnchor">Présentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fontNav" href="#realisationAnchor">Mes Réalisations</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Mes compétences</a>
+                        <a class="nav-link fontNav" href="#skillsAnchor">Mes compétences</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link fontNav" href="#contactAnchor">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -63,7 +74,12 @@
 
     </div>
 
+    <div id="presAnchor"></div>
+
     <div class="container">
+
+        <h2 class="mt-5 text-dark text-center titleh2">Présentation</h2>
+
         <p class="text-center textIntro mt-5">Intéressée depuis plusieurs années aux langages de programmation ,
             J’ai depuis intégré la formation de développeuse web et web mobile à l’école La Manu du Havre en septembre 2020 et ce,
             jusqu’en mars 2021.</p>
@@ -75,38 +91,40 @@
         </p>
     </div>
 
+    <div id="realisationAnchor"></div>
+
     <div class="myWork mt-5 text-center">
 
         <!-- <div>
             <img src="assets/img/web.svg" alt="web" class="logoWeb mx-auto">
         </div> -->
 
-        <h2 class="text-dark mb-5">Mes Réalisations</h2>
+        <h2 class="text-dark mb-5 titleh2">Mes Réalisations</h2>
 
 
         <main class=" container page-content">
-            <div class="card">
+            <div class="card cardProject">
                 <div class="content">
                     <h2 class="title cardTitle">Kansai no Bouken</h2>
                     <p class="copy">Site réalisé pour le passage du titre de développeuse web et web mobile - En cours</p>
                     <button class="btn">Allez sur le site</button>
                 </div>
             </div>
-            <div class="card">
+            <div class="card cardProject">
                 <div class="content">
                     <h2 class="title cardTitle">Sword Shop</h2>
                     <p class="copy">Site WordPress réalisé en formation</p>
                     <a class="btn" href="http://wp-lhp5-clouvel.manusien-ecolelamanu.fr/" target="_blank">Allez sur le site</a>
                 </div>
             </div>
-            <div class="card">
+            <div class="card cardProject">
                 <div class="content">
                     <h2 class="title cardTitle">projet en cours</h2>
                     <p class="copy">Bientôt disponible</p>
                     <button class="btn">Allez sur le site</button>
                 </div>
             </div>
-            <div class="card">
+            <div class="card cardProject">
                 <div class="content">
                     <h2 class="title cardTitle">projet en cours</h2>
                     <p class="copy">Bientôt disponible</p>
@@ -117,17 +135,27 @@
 
     </div>
 
-    <div class="otherWork mt-5 text-center">
+    <!-- <div class="otherWork mt-5 text-center">
 
         <h3 class="text-dark">Autres réalisations...</h3>
 
-        <p class="mt-5">Prochainement...</p>
+        <div class="row row-cols-1 row-cols-md-2 g-4" data-masonry='{"percentPosition": true }'>
+            <div class="col">
+                <div class="card bg-image hover-overlay ripple shadow-1-strong rounded" data-mdb-ripple-color="light">
+                    <a data-lightbox="galleryPortfolio" href="assets/img/chocolaterie.png" class="card-img-top imgGallery w-100 hover-shadow" alt="site de vente de chocolat">
+                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
+                    </a>
+                </div>
+            </div>
+        </div>
 
-    </div>
+    </div> -->
+
+    <div id="skillsAnchor"></div>
 
     <div class="container skills mt-5">
 
-        <h2 class="text-dark text-center">Langages informatiques</h2>
+        <h2 class="text-dark text-center titleh2">Langages informatiques</h2>
 
 
         <div class="row d-flex justify-content-center my-5">
@@ -148,44 +176,28 @@
         </div>
     </div>
 
+    <div id="contactAnchor"></div>
+
     <div class="contact mt-5 text-center pb-5">
 
-        <h2 class="text-light pt-5">Formulaire de contact</h2>
+        <h2 class="text-light pt-5 titleh2">contactez-moi</h2>
 
-        <form class="col-6 container mt-5">
-            <div class="form-outline mb-4">
-                <input type="text" id="lastname" class="form-control" />
-                <label class="form-label" for="lastname">Nom</label>
-            </div>
-
-            <div class="form-outline mb-4">
-                <input type="text" id="firstname" class="form-control" />
-                <label class="form-label" for="firstname">Prénom</label>
-            </div>
-
-            <div class="form-outline mb-4">
-                <input type="email" id="email" class="form-control" />
-                <label class="form-label" for="email">E-mail</label>
-            </div>
-
-            <div class="form-outline mb-4">
-                <textarea class="form-control" id="message" rows="4"></textarea>
-                <label class="form-label" for="message">Votre Message</label>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Envoyer</button>
-        </form>
+        <div class="text-light textIntro pt-5">
+            <p>Si vous êtes intéressé par mon profil n'hésitez pas à me contacter par mail : </p>
+            <p class="d-flex justify-content-center"><a href="mailto:celine.louvel.76@outlook.fr"><img src="assets/img/e-mail.svg" alt="mail" class="logoMail" title="cliquez pour m'nvoyer un mail"></a></p>
+        </div>
 
     </div>
 
 
-    <footer class="bg-light text-center text-white">
+    <footer class="bg-light text-center ">
+        <p class="text-dark textIntro mt-3">Vous pouvez également faire un tour sur mon github ou bien sur mon compte Linkedin !</p>
         <div class="container d-flex justify-content-center p-4 mb-4 pb-0">
             <a href="https://github.com/Celine-chan"><img src="assets/img/github.svg" alt="github" class="logofooter me-5"></a>
             <a href="https://www.linkedin.com/in/c%C3%A9line-louvel/"><img src="assets/img/linkedin.svg" alt="linkedin" class="logofooter"></a>
         </div>
 
-        <div class="text-center p-3" style="background-color: black;">
+        <div class="text-center text-white p-3" style="background-color: black;">
             © 2021 Louvel Céline
         </div>
     </footer>
@@ -196,6 +208,8 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.2.0/mdb.min.js"></script>
     <!-- js -->
     <script type="text/javascipt" src="assets/js/script.js"></script>
+    <script src="lightbox2-2.11.3/dist/js/lightbox.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
 </body>
 
 </html>
